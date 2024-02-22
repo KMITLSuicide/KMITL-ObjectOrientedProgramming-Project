d2c72420177df36754872f750feeaee0f3e78825
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-02-23 00:00:03 +0700

    feat(backend): 🚧 import class definitions from me, phuwit
    breaking: objects created before the great merge

--- backend/backend/definitions/controller.py
+++ backend/backend/definitions/controller.py
@@ -1,19 +1,42 @@
+from pydantic import UUID4
+from typing import List
+
+from backend.definitions.course import Course, CourseCatergory
+
 class Controller:
     def __init__(self) -> None:
-        self.__users = User
-        self.__coupons = Coupon
-        self.__payment = Payment
-        self.__category = Category
-        self.__progress = Progress
+        self.__categories: List[CourseCatergory] = []
 
-    def get_course_from_course_id_ctrl(self, course_id_input):
-        pass
+    def add_category(self, category: CourseCatergory):
+        if isinstance(category, CourseCatergory):
+            self.__categories.append(category)
+            return True
+        return False
 
-    def get_teacher_from_course(self, course_input):
-        pass
+    def get_all_courses(self):
+        courses: List[Course] = []
+        for category in self.__categories:
+            courses.extend(category.get_courses())
+        return courses
 
-    def validate_coupon(self, Course_input, coupon_id_input, teacher_input):
-        pass
+    def search_course_by_id(self, uuid: UUID4) -> Course | None:
+        for category in self.__categories:
+            matched_course = category.search_course_by_id(uuid)
+            if matched_course:
+                return matched_course
+        return None
 
-    def create_order(self):
-        pass
+    def get_all_categories(self):
+        return self.__categories
+
+    def search_category_by_id(self, uuid: UUID4):
+        for category in self.__categories:
+            if category.get_id() == uuid:
+                return category
+        return None
+
+    def search_category_by_name(self, name: str):
+        for category in self.__categories:
+            if category.get_name() == name:
+                return category
+        return None
