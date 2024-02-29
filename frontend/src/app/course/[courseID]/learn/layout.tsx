2ca9d68dc5e8501fe52c52975df331f0b2e2a368
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-02-29 11:54:55 +0700

    feat(frontend): :sparkles: course learn sidebar done

--- frontend/src/app/course/[courseID]/learn/layout.tsx
+++ frontend/src/app/course/[courseID]/learn/layout.tsx
@@ -1,8 +1,37 @@
+import CourseLearnSidebar, { SidebarCategory, SidebarItem } from "~/src/components/course/sidebar";
+import { getFrontendCourseViewData } from "~/src/lib/data/course";
+
 export default function CourseLearnLayout({
   children,
+  params
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode,
+  params: { courseID: string }
 }) {
+  const courseData =  getFrontendCourseViewData(params.courseID);
+  
+  const sidebarImagesItems = courseData.images.map((element): SidebarItem => {
+    return({
+      name: element.name,
+      link: `/course/${params.courseID}/learn/image/${element.id}`
+    });
+  });
+  const sidebarQuizItems = courseData.quizes.map((element): SidebarItem => {
+    return({
+      name: element.name,
+      link: `/course/${params.courseID}/learn/quiz/${element.id}`
+    });
+  });
+
+  const sidebarCategories: SidebarCategory[] = [{
+    name: 'Images',
+    sidebarItems: sidebarImagesItems
+  }, {
+    name: 'Quizes',
+    sidebarItems: sidebarQuizItems
+  }
+]
+
   return(
     <div className="flex w-full justify-center">
       <div className="flex w-full max-w-screen-xl space-x-6">
@@ -11,6 +40,7 @@ export default function CourseLearnLayout({
         </div>
         <div className="w-1/5 p-6">
           <p>im a sidebar</p>
+          <CourseLearnSidebar sidebarCategories={sidebarCategories} />
         </div>
       </div>
     </div>
