ea6c60250578bf5cc6373f2a50ccd7ddc92c6760
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-04 00:15:45 +0700

    feat(frontend): :sparkles: login and register page

---
+++ frontend/src/app/account/login/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Input } from "~/src/components/ui/input";
+import { toast } from "~/src/components/ui/use-toast";
+import { Toaster } from "~/src/components/ui/toaster";
+import { Button } from "~/src/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/src/components/ui/form";
+import Link from "next/link";
+
+const FormSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 character",
+  }),
+})
+
+export default function LogIn() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+   
+  return(
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="h-fit w-fit p-8 bg-secondary rounded-lg space-y-4">
+        <h1 className="font-extrabold text-2xl">Log in</h1>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl className="w-72">
+                    <Input type="email" {...field}></Input>
+                  </FormControl>
+                  <FormMessage className="font-semibold" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl className="w-72">
+                    <Input type="password" {...field}></Input>
+                  </FormControl>
+                  <FormMessage className="font-semibold" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Log in</Button>
+          </form>
+        </Form>
+
+        <Button variant='link' asChild>
+          <Link href='/account/register'>Register</Link>
+        </Button>
+      </div>
+      <Toaster />
+    </div>
+  )
+}
---
+++ frontend/src/app/account/register/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Input } from "~/src/components/ui/input";
+import { toast } from "~/src/components/ui/use-toast";
+import { Button } from "~/src/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/src/components/ui/form";
+import Link from "next/link";
+import { RadioGroup, RadioGroupItem } from "~/src/components/ui/radio-group";
+import { Toaster } from "~/src/components/ui/toaster";
+
+const FormSchema = z.object({
+  type: z.union([z.literal('user'), z.literal('teacher')]),
+  name: z.string().min(5, {
+    message: "Your name must be at least 5 character",
+  }),
+  email: z.string().email({
+    message: "Invalid email.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 character",
+  }),
+})
+
+export default function Register() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      type: "user",
+      name: "",
+      email: "",
+      password: "",
+    },
+  })
+  
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+   
+  return(
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="h-fit w-fit p-8 bg-secondary rounded-lg space-y-4">
+        <h1 className="font-extrabold text-2xl">Register</h1>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+          <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Are you a...</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="user" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Learner
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="teacher" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Teacher
+                    </FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl className="w-72">
+                    <Input type="text" {...field}></Input>
+                  </FormControl>
+                  <FormMessage className="font-semibold" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl className="w-72">
+                    <Input type="email" {...field}></Input>
+                  </FormControl>
+                  <FormMessage className="font-semibold" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl className="w-72">
+                    <Input type="password" {...field}></Input>
+                  </FormControl>
+                  <FormMessage className="font-semibold" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Register</Button>
+          </form>
+        </Form>
+
+        <Button variant='link' asChild>
+          <Link href='/account/login'>Log in</Link>
+        </Button>
+      </div>
+
+      <Toaster />
+    </div>
+  )
+}
