eb5a71663006aa23118472b573253660d30a5c65
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-04 00:22:55 +0700

    feat(frontend): :sparkles: move account type to be in one row

--- frontend/src/app/account/register/page.tsx
+++ frontend/src/app/account/register/page.tsx
@@ -72,7 +72,7 @@ export default function Register() {
                 <RadioGroup
                   onValueChange={field.onChange}
                   defaultValue={field.value}
-                  className="flex flex-col space-y-1"
+                  className="flex flex-row space-x-4"
                 >
                   <FormItem className="flex items-center space-x-3 space-y-0">
                     <FormControl>
