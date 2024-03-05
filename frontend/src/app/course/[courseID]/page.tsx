76b68fc3c6e43a0709ffd0e11a24a09faf569e61
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-05 17:55:27 +0700

    using data from api om course page

--- frontend/src/app/course/[courseID]/page.tsx
+++ frontend/src/app/course/[courseID]/page.tsx
@@ -2,7 +2,7 @@
 
 import { ChevronsUpDown } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "~/src/components/ui/button";
 import {
   Collapsible,
@@ -11,42 +11,44 @@ import {
 } from "~/src/components/ui/collapsible";
 import { ScrollArea } from "~/src/components/ui/scroll-area";
 import { Config } from "~/src/config";
+import { getCourseDataWithIDFromAPI } from "~/src/lib/data/course";
+import { type Course } from "~/src/lib/definitions/course";
 
-export function getFrontendCourseViewData(courseID: string) {
-  const imageNumber = (courseID.charCodeAt(0) % 6) + 1;
-  const quizNumber = courseID.charCodeAt(0);
-  return {
-    id: courseID,
-    name: "course name",
-    description: "course description",
-    category: {
-      courses: [],
-      id: "category id",
-      name: "category name",
-    },
-    price: 10000,
-    images: [
-      {
-        id: courseID + `image-${imageNumber}`,
-        name: "image name",
-        description: "image description",
-        url: `/course/react/image-${imageNumber}.png`,
-      },
-    ],
-    quizes: [
-      {
-        id: courseID + `quiz-${quizNumber}`,
-        name: "quiz name",
-        description: "quiz description",
-        questions: [
-          {
-            question: `quiz-${quizNumber}`,
-          },
-        ],
-      },
-    ],
-  };
-}
+// export function getFrontendCourseViewData(courseID: string) {
+//   const imageNumber = (courseID.charCodeAt(0) % 6) + 1;
+//   const quizNumber = courseID.charCodeAt(0);
+//   return {
+//     id: courseID,
+//     name: "course name",
+//     description: "course description",
+//     category: {
+//       courses: [],
+//       id: "category id",
+//       name: "category name",
+//     },
+//     price: 10000,
+//     images: [
+//       {
+//         id: courseID + `image-${imageNumber}`,
+//         name: "image name",
+//         description: "image description",
+//         url: `/course/react/image-${imageNumber}.png`,
+//       },
+//     ],
+//     quizes: [
+//       {
+//         id: courseID + `quiz-${quizNumber}`,
+//         name: "quiz name",
+//         description: "quiz description",
+//         questions: [
+//           {
+//             question: `quiz-${quizNumber}`,
+//           },
+//         ],
+//       },
+//     ],
+//   };
+// }
 
 export default function CourseView({
   params,
@@ -55,21 +57,30 @@ export default function CourseView({
 }) {
   const [courseContentOpen, setCourseContentOpen] = React.useState(false);
   const [courseQuizOpen, setCourseQuizOpen] = React.useState(false);
-  const courseData = getFrontendCourseViewData(params.courseID);
+  const [courseData, setCourseData] = React.useState<Course | null>(null);
+
+  useEffect(() => {
+    async function fetchData(courseID: string) {
+      const apiData = await getCourseDataWithIDFromAPI(courseID);
+      console.log(apiData);
+      setCourseData(apiData);
+    }
+    void fetchData(params.courseID);
+  }, []);
 
   return (
     <div className="flex h-full w-full justify-center">
       <div className="flex w-full max-w-screen-xl justify-center">
         <div className="flex w-3/5 flex-col space-y-4">
-          <p className="w-fit rounded-full bg-secondary p-2 px-4">
-            {courseData.category.name}
-          </p>
+          {/* <p className="w-fit rounded-full bg-secondary p-2 px-4">
+            {courseData?.}
+          </p> */}
 
           <h1 className="text-3xl">
-            <b>{courseData.name}</b>
+            <b>{courseData?._Course__name}</b>
           </h1>
 
-          <p className="text-lg">{courseData.description}</p>
+          <p className="text-lg">{courseData?._Course__description}</p>
 
           <div className="flex w-full flex-col justify-normal space-y-3">
             <h2 className="text-xl">
@@ -94,10 +105,10 @@ export default function CourseView({
               </div>
               <CollapsibleContent className="space-y-2">
                 <ul className="ml-10 list-decimal">
-                  {courseData.images.map((element, index) => {
+                  {courseData?._Course__images.map((element, index) => {
                     return (
                       <li key={index} className="rounded-md px-2 text-sm">
-                        {element.name}
+                        {element._CourseMaterial__name}
                       </li>
                     );
                   })}
@@ -122,10 +133,10 @@ export default function CourseView({
             </div>
             <CollapsibleContent className="space-y-2">
               <ul className="ml-10 list-decimal">
-                {courseData.quizes.map((element, index) => {
+                {courseData?._Course__quizes.map((element, index) => {
                   return (
                     <li key={index} className="rounded-md px-2 text-sm">
-                      {element.name}
+                      {element._CourseMaterial__name}
                     </li>
                   );
                 })}
@@ -137,7 +148,7 @@ export default function CourseView({
           <div className="flex flex-col items-center justify-center space-y-4">
             <Image
               className="rounded-md"
-              src={courseData.images[0]?.url ?? "/notexture.png"}
+              src={courseData?._Course__images[0]?._CourseMaterialImage__url ?? "/notexture.png"}
               alt="course hero image"
               width={1920}
               height={1080}
@@ -147,7 +158,7 @@ export default function CourseView({
             <div className="flex items-center justify-center rounded-md p-4 text-xl outline">
               <h3 className="text-2xl">
                 <b>
-                  {courseData.price.toLocaleString(Config.locale, {
+                  {courseData?._Course__price.toLocaleString(Config.locale, {
                     style: "currency",
                     currency: Config.currency,
                     minimumFractionDigits: 0,
--- frontend/src/lib/data/api.ts
+++ frontend/src/lib/data/api.ts
@@ -1,19 +1,6 @@
 import axios, { AxiosHeaders } from 'axios';
 import { env } from '~/src/env';
 
-// axios.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
 function getHeaders(): AxiosHeaders {
   if (typeof window !== 'undefined') {
     const token = window.localStorage.getItem('token');
