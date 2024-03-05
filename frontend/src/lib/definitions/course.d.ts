17bc1b9c05629cd21628c2ff4a37a8588a9c57e5
Dangkeys <TajdangZaa@gmail.com>
2024-03-05 15:46:44 +0700

    Added user

--- frontend/src/lib/definitions/course.d.ts
+++ frontend/src/lib/definitions/course.d.ts
@@ -1,35 +1,44 @@
-declare namespace Frontend {
-  interface CourseQuestion {
-    question: string;
-  }
+import { User } from "./user";
+interface CourseQuestion {
+  question: string;
+  correct: string;
+}
+interface CourseMaterial {
+  id: string;
+  name: string;
+  description: string;
+}
 
-  interface CourseMaterial {
-    id: string;
-    name: string;
-    description: string;
-  }
+interface CourseMaterialImage extends CourseMaterial {
+  url: string;
+}
 
-  interface CourseMaterialImage extends CourseMaterial {
-    url: string;
-  }
+interface CourseMaterialQuiz extends CourseMaterial {
+  questions: CourseQuestion[];
+}
+interface CourseMaterialVideo extends CourseMaterial{
 
-  interface CourseMaterialQuiz extends CourseMaterial {
-    questions: CourseQuestion[];
-  }
+}
+interface CourseReview
+{
+  _CourseReview__reviewer: User;
+  _CourseReview__star: number;
+  _CourseReview__comment: string
+}
+interface CourseMaterialVideo{}
+export interface Course {
+  _Course__id: string;
+  _Course__name: string;
+  _Course__description: string;
+  _Course__price: number;
+  _Course__quizes: CourseMaterialQuiz[];
+  _Course__images: CourseMaterialImage[];
+  _Course__reviews : CourseReview[];
+  _Course__latest_video : null | CourseMaterialVideo;
+}
 
-  interface Course {
-    id: string;
-    name: string;
-    description: string;
-    category: CourseCatergory;
-    price: number;
-    images: CourseMaterialImage[];
-    quizes: CourseMaterialQuiz[];
-  }
-
-  interface CourseCatergory {
-    id: string;
-    name: string;
-    courses: Course[];
-  }
+interface CourseCategory {
+  _CourseCategory__id: string;
+  _CourseCategory__name: string;
+  _CourseCategory__courses: Course[];
 }
