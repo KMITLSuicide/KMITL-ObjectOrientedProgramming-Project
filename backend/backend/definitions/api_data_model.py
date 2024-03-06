6e5f50b847ea663019420a69b5198e157cc8ce6f
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-06 13:55:54 +0700

    feat(backend): :sparkles: get course info returning a custom data model

--- backend/backend/definitions/api_data_model.py
+++ backend/backend/definitions/api_data_model.py
@@ -8,3 +8,16 @@ class CourseCardData(BaseModel):
     price: float
     rating: float
     banner_image: str
+
+class CourseInfo(BaseModel):
+    id: str
+    name: str
+    description: str
+    category_id: str
+    category_name: str
+    price: float
+    rating: float
+    banner_image: str
+    materials_images: list[str]
+    materials_quizes: list[str]
+    materials_videos: list[str]
