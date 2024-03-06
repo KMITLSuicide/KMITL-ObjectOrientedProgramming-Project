cacdd4ce23964632d1bbdce7b1d35cf4b0d1b415
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-06 10:13:59 +0700

    refactor(backend): :recycle: move get account and returns a custom model

--- backend/backend/routers/user.py
+++ backend/backend/routers/user.py
@@ -1,8 +1,10 @@
 from enum import Enum
-from typing import List
-from fastapi import APIRouter
+from typing import List, Annotated, Literal
+from fastapi import APIRouter, Depends
 
 from backend.controller_instance import controller
+from backend.definitions.user import User, Teacher
+from backend.lib.authentication import get_current_user
 
 
 router = APIRouter()
@@ -17,3 +19,23 @@ def get_all_user():
 @router.get("/teacher", tags=route_tags)
 def get_all_teacher():
     return controller.get_all_teacher()
+
+
+class AccountInfo(BaseException):
+    type: Literal['user', 'teacher']
+    id: str
+    name: str
+    email: str
+
+@router.get("/account", tags=route_tags)
+async def get_my_account_info(current_user: Annotated[User, Depends(get_current_user)]):
+    user_type = 'user'
+    if isinstance(current_user, Teacher):
+        user_type = 'teacher'
+
+    return {
+        "type": user_type,
+        "id": str(current_user.get_id()),
+        "name": current_user.get_name(),
+        "email": current_user.get_email(),
+        }
