b75c6442572a82da767853e327cdd3d26adb0726
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-07 16:17:24 +0700

    feat(frontend): :sparkles: account icon using lucide

--- frontend/src/components/navbar/account-icon.tsx
+++ frontend/src/components/navbar/account-icon.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { UserRoundX, UserRoundCheck } from "lucide-react";
+import { useEffect, useState } from "react";
 import {
   Avatar,
   AvatarFallback,
@@ -14,14 +18,30 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "~/src/components/ui/dropdown-menu";
+import { getAccountDataFromAPI } from "~/src/lib/data/account";
+import { type AccountInfo } from "~/src/lib/definitions/account";
 
 export default function AccountIcon() {
+  const [accountData, setAccountData] = useState<AccountInfo | null>(null);
+
+  useEffect(() => {
+    void getAccountDataFromAPI().then((data) => {
+      setAccountData(data);
+    });
+  }, []);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="/default_avatar.png" alt="Profile picture" />
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full outline outline-1 outline-secondary">
+          <Avatar className="h-8 w-8  items-center justify-center flex  ">
+            <AvatarImage src="/default_avatar.png" asChild>
+              {accountData ? (
+                <UserRoundCheck color="#6ee7b7" className="h-6 w-6" size={24} />
+              ) : (
+                <UserRoundX color="#fca5a5" className="h-6 w-6" size={24} />
+              )}
+            </AvatarImage>
             <AvatarFallback>PY</AvatarFallback>
           </Avatar>
         </Button>
