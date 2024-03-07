7fcf6e10f13acd2778a2aeb59ec921927e680ee8
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-07 11:47:53 +0700

    feat(frontend): :sparkles: play videos with react-player

--- frontend/src/app/course/[courseID]/learn/video/[videoID]/page.tsx
+++ frontend/src/app/course/[courseID]/learn/video/[videoID]/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { getCourseLearnDataFromAPI } from "~/src/lib/data/course";
 import { type CourseLearn } from "~/src/lib/definitions/course";
+import ReactPlayer from 'react-player';
+import { Skeleton } from "~/src/components/ui/skeleton";
 
 
 export default function CourseMaterialImage({
@@ -13,6 +15,7 @@ export default function CourseMaterialImage({
   const [learnData, setLearnData] = useState<CourseLearn | null | undefined>(
     undefined,
   );
+
   useEffect(() => {
     void getCourseLearnDataFromAPI(params.courseID).then((data) => {
       setLearnData(data);
@@ -25,11 +28,8 @@ export default function CourseMaterialImage({
   );
 
   return (
-    <div className="space-y-2">
-      <video width="1280" controls preload="none">
-        <source src={thisVideo?.url}  />
-        Your browser does not support the video tag.
-      </video>
+    <div className="space-y-2 h-full flex flex-col">
+      {learnData && <ReactPlayer url={thisVideo?.url} controls={true} width='100%' height='640px' />}
       <h1 className="text-3xl font-bold my-8">{thisVideo?.name}</h1>
       <p>{thisVideo?.description}</p>
     </div>
