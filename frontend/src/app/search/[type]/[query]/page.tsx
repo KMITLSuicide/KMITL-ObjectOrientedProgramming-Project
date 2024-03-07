cc27158e9897b122b6abdd79a527ea5f501ba62c
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-07 15:02:47 +0700

    refactor(frontend): :wastebasket: clean up another depedencies

--- frontend/src/app/search/[type]/[query]/page.tsx
+++ frontend/src/app/search/[type]/[query]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CourseCard } from "~/src/components/course/card";
 import { Card, CardHeader, CardTitle } from "~/src/components/ui/card";
