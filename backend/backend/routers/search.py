eb385187894ee0d99be80e0e5b3876b2cb3bc7d0
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-07 20:49:08 +0700

    feat(backend): :sparkles: return teacher name and id with card data

--- backend/backend/routers/search.py
+++ backend/backend/routers/search.py
@@ -25,7 +25,7 @@ def get_search_course(course_name: str):
     search_results: List[CourseCardData] = []
     courses = controller.search_course_by_name(course_name)
     for course in courses:
-        search_results.append(            
+        search_results.append(
             CourseCardData(
                 id=str(course.get_id()),
                 name=course.get_name(),
@@ -53,25 +53,4 @@ def get_search_teacher(teacher_name: str):
     teachers = controller.search_teacher_by_name(teacher_name)
     for teacher in teachers:
         search_results.append(SearchResults(name=teacher.get_name(), id=str(teacher.get_id())))
-    return search_results
-
-
-
-@router.get("/get_courses_for_teacher/{teacher_id}", tags=route_tags)
-def get_courses_for_teacher(teacher_id: UUID):
-    search_results: List[CourseCardData] = []
-    teacher = controller.get_teacher_by_id(teacher_id)
-    if not isinstance(teacher, Teacher):
-        return "Error teacher is not instance of Teacher"
-    for course in teacher.get_my_teachings():
-        search_results.append(            
-            CourseCardData(
-                id=str(course.get_id()),
-                name=course.get_name(),
-                description=course.get_description(),
-                price=course.get_price(),
-                rating=0,
-                banner_image=course.get_banner_image_url(),
-            )
-        )
     return search_results
