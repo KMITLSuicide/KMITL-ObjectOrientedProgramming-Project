13c0c80044c7c9562d35d05d3dea297108ca467b
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-10 19:03:28 +0700

    feat(frontend): :sparkles: frontend review

--- frontend/src/lib/data/course.ts
+++ frontend/src/lib/data/course.ts
@@ -4,6 +4,8 @@ import type {
   CourseLearn,
   CourseCreatePostData,
   CourseCardData,
+  Review,
+  PostReviewData,
 } from "~/src/lib/definitions/course";
 import api from "~/src/lib/data/api";
 
@@ -40,6 +42,36 @@ export async function getCourseInfoFromAPI(courseID: string) {
 export async function createCourseToAPI(data: CourseCreatePostData) {
   try {
     const response = await api.post<CourseCardData>("/course", data);
+
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function getReviews(courseID: string) {
+  try {
+    const response = await api.get<Review[]>(`/course/${courseID}/review`);
+
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function createReviews(courseID: string, data: PostReviewData) {
+  try {
+    const response = await api.post<Review[]>(`/course/${courseID}/create_review`, data);
 
     if (response.status == 200) {
       return response.data;
