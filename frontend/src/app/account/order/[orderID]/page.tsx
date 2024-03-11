9c79402769b6be6cb1df8b7704ca71a1f95849b6
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-03-11 21:39:54 +0700

    feat(frontend): :sparkles: order page

--- frontend/src/app/account/order/[orderID]/page.tsx
+++ frontend/src/app/account/order/[orderID]/page.tsx
@@ -1,24 +1,39 @@
-import { Config } from "~/src/config";
+'use client';
 
-export default function ViewOrder({ params }: { params: { orderID: string } }) {
-  const course = {
-    id: "orderid_ahdsflhskfjhklsdhfjsdhl",
-    course_list: ["course1", "course2", "course3"],
-    price: 10000,
-    address: "place",
-    payment_method: "visa",
-    status: true,
-  };
+import { useEffect, useState } from "react";
+import { toast } from "~/src/components/ui/use-toast";
+import { Config } from "~/src/config";
+import { getOrder } from "~/src/lib/data/order";
+import type { Order } from "~/src/lib/definitions/order";
+
+export default function ViewOrder({
+  params
+  } : {
+  params: { orderID: string }
+}) {
+  const [order, setOrder] = useState<Order | null | undefined>(undefined);
+  useEffect(() => {
+    void getOrder(params.orderID).then((data) => {
+      setOrder(data);
+      if (data === null) {
+        toast({
+          title: "Error",
+          description: "Failed to fetch data",
+          variant: "destructive",
+        });
+      }
+    });
+  }, [params.orderID]);
 
   return (
     <div className="flex w-full justify-center">
       <div className="w-full max-w-screen-lg space-y-4">
         <h1 className="text-3xl font-bold">Your Order</h1>
-        <p className="text-sm text-muted-foreground">{course.id}</p>
+        <p className="text-sm text-muted-foreground">{order?.id}</p>
         <div>
           <h2 className="text-xl font-bold">In this order:</h2>
           <ul className="ml-4 list-disc">
-            {course.course_list.map((courseName, i) => {
+            {order?.course_list_name.map((courseName, i) => {
               return (
                 <li key={i} className="list">
                   {courseName}
@@ -30,22 +45,22 @@ export default function ViewOrder({ params }: { params: { orderID: string } }) {
         <div className="space-y-1">
           <h2 className="text-xl font-bold">Your payment:</h2>
           <div className="flex flex-row items-center space-x-2">
-            {course.status ? (
+            {order?.status ? (
               <p className="py-2 px-4 text-sm rounded-full bg-primary text-primary-foreground w-fit">Paid</p>
               ) : (
                 <p className="py-2 px-4 text-sm rounded-full bg-destructive text-destructive-foreground w-fit">Not paid</p>
                 )}
-            <p>{course.price.toLocaleString(Config.locale, {
+            <p>{order?.price.toLocaleString(Config.locale, {
                 style: "currency",
                 currency: Config.currency,
                 minimumFractionDigits: 0,
               })}</p>
           </div>
           <div>
-            <p>Paid via {course.payment_method}</p>
+            <p>Paid via {order?.payment_method}</p>
           </div>
           <h3 className="text-lg font-semibold">Billing address:</h3>
-          <p>{course.address}</p>
+          <p>{order?.address}</p>
         </div>
       </div>
     </div>
--- frontend/src/components/order/card.tsx
+++ frontend/src/components/order/card.tsx
@@ -1,87 +1,34 @@
-import { type CourseCardData } from "~/src/lib/definitions/course";
 import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
-  CardHeader,
   CardTitle,
 } from "~/src/components/ui/card";
-import {
-  Form
-} from "~/src/components/ui/form";
-import Image from "next/image";
 import { Config } from "~/src/config";
 import Link from "next/link";
-import { Trash } from "lucide-react";
-import { Button } from "~/src/components/ui/button";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { toast } from "~/src/components/ui/use-toast";
-import { deleteCartItem } from "~/src/lib/data/cart";
-
-const FormSchema = z.object({
-  id: z.string().min(1),
-});
+import type { AllOrderItem } from "~/src/lib/definitions/order";
 
 export function OrderCard({
-  course,
-  className,
-  customLink,
-  updateCart,
+  order
 }: {
-  course: CourseCardData;
-  className?: string;
-  customLink?: string;
-  updateCart: () => void;
+  order: AllOrderItem;
 }) {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      id: course.id ?? "",
-    },
-  });
-
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    const result = await deleteCartItem(data.id);
-    if (result === null) {
-      toast({
-        title: "Error",
-        description: "Failed to delete item from cart",
-        variant: "destructive",
-      });
-    } else {
-      void updateCart();
-    }
-  }
-
   return (
     <Card
-      key={course.id}
-      className={`w-full cursor-pointer transition-colors hover:bg-secondary ${className}`}
+      className={`w-full cursor-pointer transition-colors hover:bg-secondary`}
     >
-      <div className="flex h-full w-full flex-row items-center pr-6 space-x-2">
+      <div className="flex h-full w-full flex-row items-center px-6 py-2 space-x-2">
         <Link
-          href={customLink ? customLink : `/course/${course.id}`}
+          href={`/account/order/${order.id}`}
           className="flex h-full w-full flex-row items-center"
         >
-          <CardHeader className="flex flex-col">
-            <Image
-              className="w-32 rounded-sm"
-              alt={`image of course ${course.name}`}
-              src={course.banner_image}
-              width={128}
-              height={96}
-            />
-          </CardHeader>
           <CardContent className="flex flex-grow flex-col justify-center space-y-2 p-0">
-            <CardTitle>{course.name}</CardTitle>
-            <CardDescription>{course.description}</CardDescription>
+            <CardTitle className="text-base">{order.id}</CardTitle>
           </CardContent>
           <CardFooter className="space-x-4 p-0">
             <div className="flex w-fit items-center justify-center rounded-md p-2 outline outline-1">
-              {course.price.toLocaleString(Config.locale, {
+              {order.price.toLocaleString(Config.locale, {
                 style: "currency",
                 currency: Config.currency,
                 minimumFractionDigits: 0,
@@ -89,22 +36,6 @@ export function OrderCard({
             </div>
           </CardFooter>
         </Link>
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="w-fit space-y-2"
-            >
-            <Button
-              variant="outline"
-              size="icon"
-              className="hover:bg-destructive"
-              type="submit"
-              >
-              <Trash size={24} />
-            </Button>
-          </form>
-        </Form>
       </div>
     </Card>
   );
